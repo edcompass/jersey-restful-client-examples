b3a54a6d12a6397d9bdc2ca508c364cc2cf32268
Ge Sha! <gesha@uniposta.de>
2019-01-15 22:03:12 +0100

    2nd commit

---
+++ src/test/java/eu/blky/helloworld/net/rest/jersey/JerseyClientTest.java
@@ -0,0 +1,12 @@
+package eu.blky.helloworld.net.rest.jersey;
+
+import org.junit.Test; 
+
+public class JerseyClientTest {
+
+	@Test
+	public void test() {
+		//Fail("Not yet implemented");
+	}
+
+}
