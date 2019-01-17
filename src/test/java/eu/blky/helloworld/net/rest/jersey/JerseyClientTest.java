59835c02a08361c438b144b4bf94ad0f4224e77a
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-17 16:41:57 +0100

    rest -> XML && rest -> JSON added

--- src/test/java/eu/blky/helloworld/net/rest/jersey/JerseyClientTest.java
+++ src/test/java/eu/blky/helloworld/net/rest/jersey/JerseyClientTest.java
@@ -13,17 +13,17 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response; 
-
-import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.filter.LoggingFilter;
+ 
 import org.junit.Test;
 
+import eu.blky.helloworld.net.common.Initer;
 import eu.blky.helloworld.net.rest.jersey.remote.Employee;
 import eu.blky.helloworld.net.rest.jersey.remote.Employees; 
 
 public class JerseyClientTest {
 	
     private static final int STATUS_OK = 200;
+    private static Initer i = null;
 
 	/**
      * Rigourous Test :-)
@@ -40,9 +40,9 @@ public class JerseyClientTest {
 	@Test
 	// 3. HTTP GET – Collection/List of Entities
 	public void testList() {
-		Client client = ClientBuilder.newClient( new ClientConfig().register( LoggingFilter.class ) );
-		 // http://localhost:8080/jersey-restful-client-example/rest/show-on-screen/employees
-		WebTarget webTarget = client.target("http://localhost:8080/jersey-restful-client-example/rest/").path("show-on-screen").path("employees");
+		Client client = ClientBuilder.newClient( );
+		 // http://localhost:8080/jersey-restful-client-example/rest/xml/employees
+		WebTarget webTarget = client.target("http://localhost:8080/jersey-restful-client-example/rest/").path("xml").path("employees");
 		 
 		Invocation.Builder invocationBuilder =  webTarget.request(MediaType.APPLICATION_XML);
 		Response response = invocationBuilder.get();
@@ -62,9 +62,9 @@ public class JerseyClientTest {
 	@Test
 	// 2. Jersey ClientBuilder
 	public void test() {
-		Client client = ClientBuilder.newClient( new ClientConfig().register( LoggingFilter.class ) );
-		 // http://localhost:8080/jersey-restful-client-example/rest/show-on-screen/employees/3
-		WebTarget webTarget = client.target("http://localhost:8080/jersey-restful-client-example/rest/").path("show-on-screen").path("3");
+		Client client = ClientBuilder.newClient(   );
+		 // http://localhost:8080/jersey-restful-client-example/rest/xml/employees/3
+		WebTarget webTarget = client.target("http://localhost:8080/jersey-restful-client-example/rest/").path("xml").path("3");
 		 
 		Invocation.Builder invocationBuilder =  webTarget.request(MediaType.APPLICATION_XML);
 		 
--- src/test/java/eu/blky/helloworld/net/rest/jersey/RestTest.java
+++ src/test/java/eu/blky/helloworld/net/rest/jersey/RestTest.java
@@ -2,6 +2,13 @@ package eu.blky.helloworld.net.rest.jersey;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -28,9 +35,61 @@ public class RestTest {
 	}
 
 	@Test
-	@Ignore
-	public void test() {
-		fail("Not yet implemented");
+ 
+	// http://localhost:8080/jersey-restful-client-example/rest/").path("xml").path("employees
+	public void testXML() throws IOException {
+
+		URL url = new URL("http://localhost:8080/jersey-restful-client-example/rest/xml/employees/");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/xml");
+		//conn.setRequestProperty("Accept", "application/json");
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+	}
+	
+	
+	
+	
+	
+	@Test
+	// http://localhost:8080/jersey-restful-client-example/rest/json/employees/
+	public void testJSON() throws IOException {
+
+		URL url = new URL("http://localhost:8080/jersey-restful-client-example/rest/json/employees/");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
 	}
 
 }
